f1fe4560d1c8d6a9a9cb612852a13d542bc323af
MIM <mim-oza86@mail.ru>
2024-12-03 09:18:20 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
 public class PhoneBook {
+    // Храним контакты в HashMap
+    private final Map<String, String> contacts = new HashMap<>();
+    // Для хранения имен в алфавитном порядке
+    private final TreeSet<String> names = new TreeSet<>();
+    // Метод добавления имени с номером
     public int add(String name, String phone) {
-        return 0;
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, phone);
+            names.add(name);
+        }
+        return contacts.size(); // Возвращаем количество уникальных контактов
     }
 }
